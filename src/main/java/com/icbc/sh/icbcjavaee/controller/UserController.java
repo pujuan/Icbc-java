24a058704396b4630810c305d240b668d2e31132
juanbao <921976924@qq.com>
2019-08-09 15:46:18 +0800

    08091115

--- src/main/java/com/icbc/sh/icbcjavaee/controller/UserController.java
+++ src/main/java/com/icbc/sh/icbcjavaee/controller/UserController.java
@@ -2,6 +2,7 @@ package com.icbc.sh.icbcjavaee.controller;
 
 import com.icbc.sh.icbcjavaee.dao.UserDao;
 import com.icbc.sh.icbcjavaee.entity.User;
+import com.icbc.sh.icbcjavaee.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/user")
 public class UserController {
     @Autowired
-    private UserDao userDao;
+    private UserService userService;
 
     @RequestMapping(value = "/")
     public ModelAndView index(){
@@ -30,4 +31,26 @@ public class UserController {
         return new ModelAndView("login");
     }
 
+
+    @RequestMapping(value = "/ulogin",method = RequestMethod.GET)
+    public ModelAndView ulogin(String username, String password){
+        System.out.println(username);
+        User user = userService.ulogin(username,password);
+        if(user!=null && user.getPassword().equals(password)){
+            return new ModelAndView("index");
+        } else {
+            return new ModelAndView("login");
+        }
+
+    }
+
+    @RequestMapping(value = "/uregister",method = RequestMethod.GET)
+    public ModelAndView save(String username,String password){
+        User user = userService.save(username,password);
+        if (user!=null){
+            return new ModelAndView("login");
+        }else {
+            return new ModelAndView("register");
+        }
+    }
 }
--- src/main/java/com/icbc/sh/icbcjavaee/service/UserService.java
+++ src/main/java/com/icbc/sh/icbcjavaee/service/UserService.java
@@ -4,36 +4,23 @@ import com.icbc.sh.icbcjavaee.dao.UserDao;
 import com.icbc.sh.icbcjavaee.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-@Service
-@ResponseBody
-@RequestMapping("/user")
+@Service("userService")
 public class UserService {
+
     @Autowired
     private UserDao userDao;
-    @RequestMapping(value = "/ulogin",method = RequestMethod.GET)
-    public ModelAndView ulogin(String username, String password){
-        System.out.println("用户名是："+username);
-        System.out.println("密码是："+password);
-        User user = userDao.login(username,password);
-        if(user!=null && user.getPassword().equals(password)){
-            return new ModelAndView("index");
-        } else {
-            return new ModelAndView("login");
-        }
+    public User ulogin(String username, String password){
+
+        return userDao.login(username,password);
 
     }
 
-    @RequestMapping(value = "/uregister",method = RequestMethod.GET)
-    public ModelAndView save(String username,String password){
+    public User save(String username,String password){
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         userDao.save(user);
-        return new ModelAndView("login");
+        return userDao.login(username,password);
     }
 }
