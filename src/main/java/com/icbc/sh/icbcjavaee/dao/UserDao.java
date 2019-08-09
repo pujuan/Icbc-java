fbd5df2939af872f5731a41e34d50821256187ac
juanbao <921976924@qq.com>
2019-08-09 14:34:23 +0800

    08091434

--- src/main/java/com/icbc/sh/icbcjavaee/dao/UserDao.java
+++ src/main/java/com/icbc/sh/icbcjavaee/dao/UserDao.java
@@ -1,13 +1,13 @@
 package com.icbc.sh.icbcjavaee.dao;
 
 import com.icbc.sh.icbcjavaee.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserDao extends JpaRepository<User,String> {
 
 
-    @Query("select username,password from User where username=#{username} and password = #{password}")
+    @Query("select u from User u where u.username= :username and u.password= :password")
      User login(String username,String password);
 }
